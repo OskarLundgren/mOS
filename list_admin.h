3fd939c93a921d497bb6128e36864567902abf6d
Oskar Lundgren <oskar.lundgren92@gmail.com>
2015-02-13 13:35:12 +0100

    First commit

---
+++ list_admin.h
@@ -0,0 +1,33 @@
+//
+//  linkedlist.h
+//  mOS DSTII
+//
+//  Created by Markus Axelsson and Oskar Lundgren  on 2015-02-06.
+//  Copyright (c) 2015 Markus & Oskar. All rights reserved.
+//
+
+#ifndef __mOS_DSTII__linkedlist__
+#define __mOS_DSTII__linkedlist__
+
+#include <stdio.h>
+#include "kernel.h"
+
+/** Definitions **/
+extern list *g_readylist;
+extern list *g_timerlist;
+extern list *g_waitinglist;
+
+/** Functions **/
+list* create_list(void);
+
+
+/*Inserting objects in lists*/
+int insert_timerlist(listobj *insert_object, int nTCnt);
+int insert_waiting_ready_list(list *list, listobj *insert_object);
+
+/*Extracting objects from lists*/
+listobj* extract_timerlist(void);
+void extract_waitinglist(listobj *object);
+listobj* extract_readylist(void);
+
+#endif /* defined(__mOS_DSTII__linkedlist__) */
---
+++ timing.h
@@ -0,0 +1,16 @@
+//
+//  timing.h
+//  mOS
+//
+//  Created by Markus Axelsson on 2015-02-10.
+//  Copyright (c) 2015 Markus & Oskar. All rights reserved.
+//
+
+#ifndef __mOS__timing__
+#define __mOS__timing__
+
+#include <stdio.h>
+
+void set_ticks(uint nTicks);
+
+#endif /* defined(__mOS__timing__) */
